f314943e502a7e6ed15afb0f384a9145430185cd
hraboviyvadim <hraboviyvadim@gmail.com>
2013-11-12 15:05:22 +0200

    calculating

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
 
+// calculator
+	
+	$(".go").click(function(){
+		var weight_val = $("#weight").val();
+		var volume_val = $("#volume").val();
+		var	oversized = 1;
+		var	packing = 0;
+		var packing_price = $("#packing").attr("data-price");
+		if(weight_val == ''){
+			$("#weight").addClass('error');
+		}
+		if(volume_val == ''){
+			$("#volume").addClass('error');
+		}
+		else {
+			if($("#oversized").prop("checked")){
+				var oversized = 1.333;
+			}
+			if($("#packing").prop("checked")){
+				var packing = (+volume_val*packing_price);
+			}
+			var result = (((+weight_val + +volume_val)*100)*oversized + packing);
+			$("#result").text(result);
+		} 
+	});
 
-});
+});	
